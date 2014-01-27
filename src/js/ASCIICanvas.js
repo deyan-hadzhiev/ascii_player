1236ea754403e68cb96e13dc6481714d97de8a68
Dian Nikolov <dian.nikolov91@gmail.com>
2014-01-27 11:28:43 +0200

    Added image file loading + image to ASCII functionality

--- src/js/ASCIICanvas.js
+++ src/js/ASCIICanvas.js
@@ -3,41 +3,48 @@
 var ASCIICanvas = (function () {
     "use strict";
 
-    function getCharRenderedSize () {
-            var canvasCharFontSize = $("#ascii").css("font-size");
-            var canvasLineHeight = $("#ascii").css("line-height");
+    function getCharRenderedSize() {
+        var canvasCharFontSize = $("#ascii").css("font-size"),
+            canvasLineHeight = $("#ascii").css("line-height"),
+            doc = document.body,
+            tmpDiv = document.createElement('div'),
+            attr = { fontSize: canvasCharFontSize, padding: '0', position: 'absolute',
+                lineHeight: canvasLineHeight, visibility: 'hidden', fontFamily: 'monospace' },
+            p,
+            size;
 
-            var doc = document.body;
-            var tmpDiv = document.createElement('div');
-            var atts = {fontSize: canvasCharFontSize, padding:'0', position:'absolute', lineHeight: canvasLineHeight, visibility:'hidden', fontFamily:'monospace'};
-            for(var p in atts){
-                tmpDiv.style[p]= atts[p];
-            }
-            tmpDiv.appendChild(document.createTextNode("M"));
-            doc.appendChild(tmpDiv);
-            var size = [tmpDiv.offsetWidth, tmpDiv.offsetHeight];
-            doc.removeChild(tmpDiv);
-            return size;
+        for (p in attr) {
+            tmpDiv.style[p] = attr[p];
+        }
+
+        tmpDiv.appendChild(document.createTextNode("M"));
+        doc.appendChild(tmpDiv);
+        size = [tmpDiv.offsetWidth, tmpDiv.offsetHeight];
+        doc.removeChild(tmpDiv);
+        return size;
     }
 
     function mapColorToChar(colorValue) {
-        var charSet = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ",
+        var charSet = '@0` ',
+            //charSet = '"@MBHENR#KWXDFPQASUZbdehx*8Gm&04LOVYkpq5Tagns69owz$CIu23Jcfry%1v7l+it[] {}?j|()=~!-/<>\"^_\';,:`. "',
+            //charSet = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ",
             colorInterval = 256 / charSet.length,
             i = 0;
 
-        for (i = 1; i <= charSet.length; i += 1) {
-            if (colorValue >= colorInterval * (i - 1) && colorValue < colorInterval * i) {
-                return charSet[i - 1];
+        if (typeof colorValue === "number") {
+            for (i = 1; i <= charSet.length; i += 1) {
+                if (colorValue >= colorInterval * (i - 1) && colorValue < colorInterval * i) {
+                    return charSet[i - 1];
+                }
             }
         }
 
-        return charSet[charSet.length - 1];
+        return colorValue;
     }
 
     return {
         drawFrame: function (frame) {
-            var ascii = document.getElementById("ascii"),
-                frameContent = '',
+            var frameContent = '',
                 i = 0,
                 j = 0;
 
@@ -48,7 +55,7 @@ var ASCIICanvas = (function () {
             //Draw new frame
             for (i = 0; i < frame.getHeight(); i += 1) {
                 for (j = 0; j < frame.getWidth(); j += 1) {
-                    frameContent += frame.getElement(i, j);
+                    frameContent += mapColorToChar(frame.getElement(i, j));
                 }
                 frameContent += '\n';
             }
